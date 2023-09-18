aa30e55f7b70c8be6ad4de580f9695ba084b6a60
djanyreason <djanyreason@gmail.com>
2023-09-18 13:48:49 -0400

    Completes ex. 6.1, implements and tests reducer
    This commit modifies the code of the reducer function counterReducer
    so that it modifies state appropriately based on the action type
    requested. It also adds JEST tests to tests/reducer.test.js to verify
    that the reducer function is behaving as expected

--- unicafe-redux/src/tests/reducer.test.js
+++ unicafe-redux/src/tests/reducer.test.js
@@ -1,35 +1,84 @@
-import deepFreeze from 'deep-freeze'
-import counterReducer from './reducer'
+import deepFreeze from 'deep-freeze';
+import counterReducer from '../reducers/reducer';
 
 describe('unicafe reducer', () => {
   const initialState = {
     good: 0,
     ok: 0,
     bad: 0
-  }
+  };
 
   test('should return a proper initial state when called with undefined state', () => {
-    const state = {}
+    const state = {};
     const action = {
       type: 'DO_NOTHING'
-    }
+    };
 
-    const newState = counterReducer(undefined, action)
-    expect(newState).toEqual(initialState)
-  })
+    const newState = counterReducer(undefined, action);
+    expect(newState).toEqual(initialState);
+  });
 
   test('good is incremented', () => {
     const action = {
       type: 'GOOD'
-    }
-    const state = initialState
+    };
+    const state = initialState;
 
-    deepFreeze(state)
-    const newState = counterReducer(state, action)
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
     expect(newState).toEqual({
       good: 1,
       ok: 0,
       bad: 0
-    })
-  })
+    });
+  });
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    });
+  });
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    });
+  });
+
+  test('zero resets', () => {
+    const action = {
+      type: 'ZERO'
+    };
+    const state = {
+      good: 86,
+      ok: 75,
+      bad: 309
+    };
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    });
+  });
 })
