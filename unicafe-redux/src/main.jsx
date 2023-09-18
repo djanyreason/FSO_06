a513cda574daa17d06d7162d26ee37cbb13dd687
djanyreason <djanyreason@gmail.com>
2023-09-18 13:55:57 -0400

    Completes ex 6.2, implements basic app function
    This commit implements the Redux app in a basic format - four buttons
    each corresponding to one commmand, and three lines showing the
    current state of the three parts of the state object.

--- unicafe-redux/src/main.jsx
+++ unicafe-redux/src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { createStore } from 'redux';
-import reducer from './reducers/reducer';
+import counterReducer from './reducers/reducer';
 
-const store = createStore(reducer);
+const store = createStore(counterReducer);
 
 const App = () => {
   const good = () => {
@@ -15,13 +15,13 @@ const App = () => {
 
   return (
     <div>
-      <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={e => store.dispatch({ type: 'GOOD' })}>good</button>
+      <button onClick={e => store.dispatch({ type: 'OK' })}>ok</button>
+      <button onClick={e => store.dispatch({ type: 'BAD' })}>bad</button>
+      <button onClick={e => store.dispatch({ type: 'ZERO' })}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   );
 };
