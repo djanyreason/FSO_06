70c6ed625028aea9c2e06a56b8e5ad4a1340578e
djanyreason <djanyreason@gmail.com>
2023-09-19 09:55:50 -0400

    Completes ex 6.3, implements voting for anecdotes
    This commit implements the functionality to vote for anecdotes by
    clicking the vote button. This functionality is enabled through the
    reducer function and the vote totals are saved in Redux store.

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,15 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state);
   console.log('action', action);
 
-  return state;
+  switch (action.type) {
+    case 'VOTE': 
+      return state.map(
+        dote => dote.id !== action.payload.id
+        ? dote
+        : { ...dote, votes: (dote.votes+1)}
+      );
+    default: return state;
+  }
 };
 
 export default reducer;
