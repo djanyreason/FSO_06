3c961a48fac754c85b74d87d1f8b8702f7c72f94
djanyreason <djanyreason@gmail.com>
2023-09-20 17:49:29 -0400

    Completes ex 6.20, retrieves initial data from DB
    This commit modifies the application so that it retrieves the initial
    list of anecdotes from the backend database server, using React
    Query.

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,5 +1,8 @@
+import { useQuery } from '@tanstack/react-query';
+
 import AnecdoteForm from './components/AnecdoteForm';
 import Notification from './components/Notification';
+import { getAnecdotes } from './requests';
 
 const App = () => {
 
@@ -7,13 +10,17 @@ const App = () => {
     console.log('vote');
   };
 
-  const anecdotes = [
-    {
-      'content': 'If it hurts, do it more often',
-      'id': '47145',
-      'votes': 0
-    },
-  ];
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false
+  });
+
+  if(result.isLoading) return <div>loading data...</div>;
+  else if(result.isError)
+    return <div>anecdote service not available due to problems in server</div>;
+
+  const anecdotes = result.data;
 
   return (
     <div>
